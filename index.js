04d08f5cc97e053b4b1502b92dd4f81845aa0d3b
intr0vert <intr0vert.noname@mail.ru>
2020-02-25 22:24:18 +0300

    Added connect\disconnect emmit

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function(socket) {
-  socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
-  });
+io.on("connection", socket => {
+    socket.broadcast.emit("user connected");
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user disconnected");
+    });
+    socket.on("chat message", msg => {
+        io.emit("chat message", msg);
+    });
 });
 
-http.listen(3000, function() {
-  console.log("listening on *:3000");
+http.listen(3000, () => {
+    console.log("listening on *:3000");
 });
